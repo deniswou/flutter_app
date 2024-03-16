718df4c714be2cd1dfa598fc802dc5609bca5d88
deniswou <deniswouh@gmail.com>
2024-03-16 19:29:58 -0300

    add avatar components

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -8,6 +8,29 @@ class AvatarPage extends StatelessWidget{
       appBar: AppBar(
         title: Text('Avatar Page', style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.blue,
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://deanagar.com.au/wp-content/uploads/2021/07/072-men-photography.jpg'),
+              radius: 20.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('DC'),
+              backgroundColor: Colors.greenAccent,
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://deanagar.com.au/wp-content/uploads/2021/07/072-men-photography.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        )
       ),
     );
   }
