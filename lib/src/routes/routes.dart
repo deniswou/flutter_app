9cd710671ffac697a6f6204ad01b3916db4ee0fe
deniswou <deniswouh@gmail.com>
2024-03-16 19:57:52 -0300

    add animated container

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/src/pages/alert_page.dart';
+import 'package:flutter_app/src/pages/animated_container.dart';
 import 'package:flutter_app/src/pages/avatar_page.dart';
 import 'package:flutter_app/src/pages/card_page.dart';
 import 'package:flutter_app/src/pages/home_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes(){
 return <String, WidgetBuilder>{
-        '/'       : (BuildContext context) => HomePage(),
-        'alert'   : (BuildContext context) => AlertPage(),
-        'avatar'  : (BuildContext context) => AvatarPage(),
-        'card'  : (BuildContext context) => CardPage(),
+        '/'                 : (BuildContext context) => HomePage(),
+        'alert'             : (BuildContext context) => AlertPage(),
+        'avatar'            : (BuildContext context) => AvatarPage(),
+        'card'              : (BuildContext context) => CardPage(),
+        'animatedContainer' : (BuildContext context) => AnimatedContainerPage(),
 };
 }
 
