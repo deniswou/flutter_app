1b218ad8c6bb1fdc7452352ff7ff6372aa7ff2f8
deniswou <deniswouh@gmail.com>
2024-03-16 19:19:04 -0300

    add alert components

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -9,6 +9,13 @@ class AlertPage extends StatelessWidget{
         title: Text('Alert Page', style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.blue,
       ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Mostrar alerta', style: TextStyle(color: Colors.white)),
+          style: ElevatedButton.styleFrom(backgroundColor: Colors.blue, elevation: 6.0),
+          onPressed: () => _mostrarAlert(context),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_back),
         backgroundColor: Colors.blue,
@@ -16,6 +23,39 @@ class AlertPage extends StatelessWidget{
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  void _mostrarAlert(BuildContext context){
+    showDialog(
+      context: context, 
+      barrierDismissible: true,
+      builder: (context){
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          title: Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Este es el contenido de la alerta'),
+              FlutterLogo(size: 100.0,),
+            ]
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancelar'),
+              onPressed: () => Navigator.of(context).pop(), 
+            ),
+            TextButton(
+              child: Text('Aceptar'),
+              onPressed: (){
+                Navigator.of(context).pop();
+              }, 
+            ),
+          ],
+          actionsAlignment: MainAxisAlignment.center,
+        );
+      },
     );
   }
 }
